9043eb48b520cc2f2c82735d92c9a63650aeacd5
Minagami-Yuiki <yuishiki.ryougi@gmail.com>
2025-11-17 17:34:22 +0800

    submit img video

---
+++ static/js/event_handler.js
@@ -0,0 +1,383 @@
+document.addEventListener('DOMContentLoaded', domReady);
+
+        function domReady() {
+            new Dics({
+                container: document.querySelectorAll('.b-dics')[0],
+                hideTexts: false,
+                textPosition: "bottom"
+
+            });
+            new Dics({
+                container: document.querySelectorAll('.b-dics')[1],
+                hideTexts: false,
+                textPosition: "bottom"
+
+            });
+            new Dics({
+                container: document.querySelectorAll('.b-dics')[2],
+                hideTexts: false,
+                textPosition: "bottom"
+
+            });
+            new Dics({
+                container: document.querySelectorAll('.b-dics')[3],
+                hideTexts: false,
+                textPosition: "bottom"
+
+            });
+            new Dics({
+                container: document.querySelectorAll('.b-dics')[4],
+                hideTexts: false,
+                textPosition: "bottom"
+
+            });
+            new Dics({
+                container: document.querySelectorAll('.b-dics')[5],
+                hideTexts: false,
+                textPosition: "bottom"
+
+            });
+        }
+
+        function replicargbdSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[0]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 8
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/replica/rgbd/room0/';
+                        break;
+                    case 1:
+                        image.src = 'img/replica/rgbd/room1/';
+                        break;
+                    case 2:
+                        image.src = 'img/replica/rgbd/room2/';
+                        break;
+                    case 3:
+                        image.src = 'img/replica/rgbd/office0/';
+                        break;
+                    case 4:
+                        image.src = 'img/replica/rgbd/office1/';
+                        break;
+                    case 5:
+                        image.src = 'img/replica/rgbd/office2/';
+                        break;
+                    case 6:
+                        image.src = 'img/replica/rgbd/office3/';
+                        break;
+                    case 7:
+                        image.src = 'img/replica/rgbd/office4/';
+                        break;    
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'photoslam.jpg';
+                        break;
+                    case 1:
+                        image.src = image.src + 'splatam.png';
+                        break;
+                    case 2:
+                        image.src = image.src + 'monogs.png';
+                        break;
+                    case 3:
+                        image.src = image.src + 'rtgslam.png';
+                        break;
+                    case 4:
+                        image.src = image.src + 'segsslam.jpg';
+                        break;
+                    case 5:
+                        image.src = image.src + 'ours.png';
+                        break;
+                    case 6:
+                        image.src = image.src + 'gt.png';
+                        break;
+
+                }
+            }
+
+            let scene_list = document.getElementById("replica-rgbd").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
+
+        function tumrgbdSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[1]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 6
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/tum/rgbd/fr1/';
+                        break;
+                    case 1:
+                        image.src = 'img/tum/rgbd/fr2/';
+                        break;
+                    case 2:
+                        image.src = 'img/tum/rgbd/fr3/';
+                        break; 
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'photoslam.png';
+                        break;
+                    case 1:
+                        image.src = image.src + 'splatam.png';
+                        break;
+                    case 2:
+                        image.src = image.src + 'monogs.png';
+                        break;
+                    case 3:
+                        image.src = image.src + 'segsslam.jpg';
+                        break;
+                    case 4:
+                        image.src = image.src + 'ours.png';
+                        break;
+                    case 5:
+                        image.src = image.src + 'gt.png';
+                        break;
+
+                }
+            }
+
+            let scene_list = document.getElementById("tum-rgbd").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
+
+
+        function replicamonoSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[2]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 5
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/replica/mono/room0/';
+                        break;
+                    case 1:
+                        image.src = 'img/replica/mono/room1/';
+                        break;
+                    case 2:
+                        image.src = 'img/replica/mono/room2/';
+                        break;
+                    case 3:
+                        image.src = 'img/replica/mono/office0/';
+                        break;
+                    case 4:
+                        image.src = 'img/replica/mono/office1/';
+                        break;
+                    case 5:
+                        image.src = 'img/replica/mono/office2/';
+                        break;
+                    case 6:
+                        image.src = 'img/replica/mono/office3/';
+                        break;
+                    case 7:
+                        image.src = 'img/replica/mono/office4/';
+                        break;    
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'photoslam.jpg';
+                        break;
+                    case 1:
+                        image.src = image.src + 'monogs.png';
+                        break;
+                    case 2:
+                        image.src = image.src + 'segsslam.jpg';
+                        break;
+                    case 3:
+                        image.src = image.src + 'ours.png';
+                        break;
+                    case 4:
+                        image.src = image.src + 'gt.png';
+                        break;
+                }
+            }
+
+            let scene_list = document.getElementById("replica-mono").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
+
+        function tummonoSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[3]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 5
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/tum/mono/fr1/';
+                        break;
+                    case 1:
+                        image.src = 'img/tum/mono/fr2/';
+                        break;
+                    case 2:
+                        image.src = 'img/tum/mono/fr3/';
+                        break; 
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'photoslam.png';
+                        break;
+                    case 1:
+                        image.src = image.src + 'monogs.png';
+                        break;
+                    case 2:
+                        image.src = image.src + 'segsslam.jpg';
+                        break;
+                    case 3:
+                        image.src = image.src + 'ours.png';
+                        break;
+                    case 4:
+                        image.src = image.src + 'gt.png';
+                        break;
+                }
+            }
+
+            let scene_list = document.getElementById("tum-mono").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
+
+        function eurocstereoSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[4]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 4
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/euroc/stereo/mh01/';
+                        break;
+                    case 1:
+                        image.src = 'img/euroc/stereo/mh02/';
+                        break;
+                    case 2:
+                        image.src = 'img/euroc/stereo/v101/';
+                        break; 
+                    case 3:
+                        image.src = 'img/euroc/stereo/v201/';
+                        break; 
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'photoslam.png';
+                        break;
+                    case 1:
+                        image.src = image.src + 'segsslam.jpg';
+                        break;
+                    case 2:
+                        image.src = image.src + 'ours.png';
+                        break;
+                    case 3:
+                        image.src = image.src + 'gt.png';
+                        break;
+                }
+            }
+
+            let scene_list = document.getElementById("euroc-stereo").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
+
+        function replicageomSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[5]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 8
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/geom/replica/room0';
+                        break;
+                    case 1:
+                        image.src = 'img/geom/replica/room1/';
+                        break;
+                    case 2:
+                        image.src = 'img/geom/replica/room2/';
+                        break;
+                    case 3:
+                        image.src = 'img/geom/replica/office0/';
+                        break;
+                    case 4:
+                        image.src = 'img/geom/replica/office1/';
+                        break;
+                    case 5:
+                        image.src = 'img/geom/replica/office2/';
+                        break;
+                    case 6:
+                        image.src = 'img/geom/replica/office3/';
+                        break; 
+                    case 7:
+                        image.src = 'img/geom/replica/office4/';
+                        break; 
+
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'rgb_gt.png';
+                        break;
+                    case 1:
+                        image.src = image.src + 'rgb_pre.png';
+                        break;
+                    case 2:
+                        image.src = image.src + 'depth_gt.png';
+                        break;
+                    case 3:
+                        image.src = image.src + 'depth_pre.png';
+                        break;
+                    case 4:
+                        image.src = image.src + 'normal_gt.png';
+                        break;
+                    case 5:
+                        image.src = image.src + 'normal_pre.png';
+                        break;
+                }
+            }
+
+            let scene_list = document.getElementById("replica-geom").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
